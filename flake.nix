5ae936da699ad4e780705b611ba099cfc18f68c9
Volodymyr Antonov <azazaka2002@gmail.com>
2024-01-01 00:39:19 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+    esp-dev.url = "github:mirrexagon/nixpkgs-esp-dev";
+  };
+
+  outputs = { self, nixpkgs, utils, esp-dev }:
+    utils.lib.eachSystem [ "x86_64-linux" ] (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = with pkgs; mkShell {
+          buildInputs = [
+            esp-dev.packages.${system}.esp-idf-full
+            pkgs.nixpkgs-fmt
+            # pkgs.clang-tools
+            pkgs.udev
+          ];
+
+          shellHook = ''
+            export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.lib.makeLibraryPath [
+              pkgs.udev
+            ]}"
+          '';
+        };
+      });
+}
